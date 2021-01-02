23e05d3bbd1f1d7697504264aff8016e549a9273
Bekar <beqarioni@yahoo.co.uk>
2021-01-03 01:51:43 +0400

    3.19: Phonebook database, step7

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 
 const url = process.env.MONGODB_URI
@@ -13,9 +14,13 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
         console.log('error connecting to MongoDB:', error.message)
 })
 const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        unique: true
+    },
     number: String,
 })
+personSchema.plugin(uniqueValidator)
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
