bbd2358afe22930eff8d52c8935d5b936811ba84
Bekar <beqarioni@yahoo.co.uk>
2021-01-03 21:59:35 +0400

    3.21 Deploying the database backend to production

--- index.js
+++ index.js
@@ -90,9 +90,6 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
   
 const errorHandler = (error, request, response, next) => {
-    // console.error(error.message)
-    console.log(typeof error)
-
     if (error.name === 'CastError' && error.kind == 'ObjectId') {
         return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
